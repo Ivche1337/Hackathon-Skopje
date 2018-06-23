1c31d8a54c3029c738960cd1478d933632a9fc08
Bojan Sofronievski <bojan.drago@gmail.com>
2018-06-24 00:57:26 +0200

    Delete test_bot.py

--- test_bot.py
+++
@@ -1,9 +0,0 @@
-import tweepy
-import authenticator
-import twitter_client
-import tokens
-
-if __name__ == "__main__":
-	client = twitter_client.TwitterClient()
-
-	print(client.get_timeline("pycon", 5))
