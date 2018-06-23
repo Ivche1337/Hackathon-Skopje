237133507ae450d7ce286b8e785b7504f2d81a3b
Bojan <bojan@debian>
2018-06-24 00:55:00 +0200

    Improved many things and added new features

--- app.py
+++ app.py
@@ -1,38 +1,76 @@
-from flask import Flask, request, render_template
-from flask_ask import Ask, statement, question, session
+from flask import Flask, session, request, redirect, render_template, url_for
+from flask_ask import Ask, statement, question
 from twitter_client import TwitterClient
+import tweepy
+import tokens
 
 app = Flask(__name__)
 ask = Ask(app, "/twitter_skill")
 
-@app.route('/')
-@app.route('/index')
-def index():
-	return render_template('index.html')
+CALLBACK_URL = 'http://localhost:5000/verify'
+db = {}
+session = {}
+
+@app.route("/")
+def send_token():
+	auth = tweepy.OAuthHandler(tokens.CONSUMER_KEY, tokens.CONSUMER_SECRET, CALLBACK_URL)
+	redirect_url= auth.get_authorization_url()
+	session['request_token']=auth.request_token
+	
+	return redirect(redirect_url)	
+
+@app.route("/verify")
+def get_verification():
+	verifier= request.args['oauth_verifier']
+	
+	auth = tweepy.OAuthHandler(tokens.CONSUMER_KEY, tokens.CONSUMER_SECRET)
+	token = session['request_token']
+	del session['request_token']
+	
+	auth.request_token = token
+	auth.get_access_token(verifier)
+	api = tweepy.API(auth)
+	db['api']=api
+	db['access_token_key']=auth.access_token
+	db['access_token_secret']=auth.access_token_secret
+	return redirect(url_for('start'))
+
+@app.route("/start")
+def start():
+	api = db['api']
+	return("You are connected!")
 
 @ask.launch
 def new_ask():
-        return statement("Hi!")
+    return statement("Welcome to twitter lex! Please say: How can I use twitter lex, for help.")
 
 @ask.intent('ReadTweets')
 def read_post(user_id):
-	return statement(TwitterClient().get_timeline(user_id, num_of_posts = 10))
+	return statement(TwitterClient(db['access_token_key'],db["access_token_secret"]).get_timeline(user_id, num_of_posts = 20))
 
 @ask.intent('PostTweet')
 def post_tweet(tweet):
-        print(tweet)
-        return statement(TwitterClient().post_status(tweet))
+    return statement(TwitterClient(db['access_token_key'],db["access_token_secret"]).post_status(tweet))
+
+@ask.intent('StreamTweets')
+def stream_tweet(hashtag):
+	return statement(TwitterClient(db['access_token_key'],db["access_token_secret"]).get_tweets(hashtag, num_of_posts = 20))
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+	return question("You can use twitter lex to read top user tweets by saying: Read top tweets from ...; Read newest tweets with some hasthag by saying: Read lattest tweets with hashtag..., or to post new tweet by saying: Tweet for me... What would you like to do?")
 
 
 @ask.intent('AMAZON.StopIntent')
 def stop():
     return statement("Goodbye!")
 
-
 @ask.intent('AMAZON.CancelIntent')
 def cancel():
 	return statement("Okay")
 
-
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.secret_key = 'secret key'
+	app.config['SESSION_TYPE'] = 'filesystem'
+	app.debug = True
+	app.run()
--- twitter_client.py
+++ twitter_client.py
@@ -5,26 +5,38 @@ import re
 
 class TwitterClient():
 
-	def __init__(self):
+	def __init__(self, access_token, access_token_secret):
 		self.auth = authenticator.Authenticator(tokens.CONSUMER_KEY, tokens.CONSUMER_SECRET, 
-							tokens.ACCESS_TOKEN, tokens.ACCESS_TOKEN_SECRET).authenticate()
+							access_token, access_token_secret).authenticate()
 		self.api = tweepy.API(self.auth)
 
 	def get_timeline(self, user_id=None, num_of_posts=1):
 		posts = []
 		posts_string = ""
-		for status in tweepy.Cursor(self.api.user_timeline, id=user_id).items(num_of_posts):
-			post = status._json["text"]
+		users = self.api.search_users(user_id)
+		for status in tweepy.Cursor(self.api.user_timeline, tweet_mode="extended", id=users[0].id).items(num_of_posts):
+			post = status.full_text
 			post = re.sub(r"http\S+", "", post)
 			posts.append(post)
 		no = 1
 		for post in posts:
-			if len(post) >= 10:
+			if len(post) >= 10 and 'RT @' not in post:
 				posts_string += "Tweet " + str(no) + ": " + post + "..." 
 				no += 1
 
 		return posts_string
 
+	def get_tweets(self, hashtag, num_of_posts=1):
+		posts = []
+		posts_string = ""
+		for status in tweepy.Cursor(self.api.search, q=("#" + hashtag), tweet_mode="extended").items(num_of_posts):
+			post = [status.user.name, status.full_text]
+			post[1] = re.sub(r"http\S+", "", post[1])
+			posts.append(post)
+		for post in posts:
+			if 'RT @' not in post[1]:
+				posts_string += "Tweet from " + post[0] + ": " + post[1] + "..."
+		return posts_string
 
 	def post_status(self, post):
 		try:
