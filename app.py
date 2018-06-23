4f223cd206e1366e36ee84b920681eb039dc9045
Bojan <bojan@debian>
2018-06-23 11:37:48 +0200

    Test 1

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, request, render_template
-app = Flask(__name__)
+from flask_ask import Ask, statement, question, session
+from twitter_client import TwitterClient
 
+app = Flask(__name__)
+ask = Ask(app, "/twitter_skill")
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@ask.launch
+def new_ask():
+    welcome = render_template('welcome')
+    reprompt = render_template('reprompt')
+    return question(welcome) \
+        .reprompt(reprompt)
+
+@ask.intent('ReadTwitterPost')
+def readPost():
+	return statement(TwitterClient().get_timeline(num_of_posts = 3))
 
 if __name__ == '__main__':
 	app.run(debug=True)
