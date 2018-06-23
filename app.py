2625b5de3874853ce1c981a87af3fdb24a97428f
Teodora98 <mladenovskateodora29@gmail.com>
2018-06-23 14:17:35 +0200

    Some minor changes.

--- app.py
+++ app.py
@@ -12,18 +12,19 @@ def index():
 
 @ask.launch
 def new_ask():
-    welcome = render_template('welcome')
-    reprompt = render_template('reprompt')
-    return question(welcome) \
-        .reprompt(reprompt)
+        return statement("Hi!")
+#    welcome = render_template('welcome')
+#    reprompt = render_template('reprompt')
+#    return question(welcome) \
+#        .reprompt(reprompt)
 
 @ask.intent('ReadTweets')
 def read_post():
 	return statement(TwitterClient().get_timeline(num_of_posts = 3))
 
-@ask.intent('QPostTweet')
-def post_tweet():
-	return question("Do you want me to post something on your twitter feed")
+#@ask.intent('QPostTweet')
+#def post_tweet():
+#	return question("Do you want me to post something on your twitter feed")
 	
 
 if __name__ == '__main__':
